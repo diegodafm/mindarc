c1b93417ebd8a10e19c75e661f07ab446a7f2bab
Diego Mendonca <u06dxm@macys.com>
2015-06-26 23:22:37 -0300

    implementing responsive design

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ var express  = require('express'),
     router = express.Router(),
     bodyParser = require('body-parser'),
     app = express(),
-    port = process.env.PORT || 5000;
+    port = process.env.PORT || 5001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/'));
 
-app.listen(process.env.PORT || 5000, function(){
+app.listen(port, function(){
     console.log('Express server listening on port ' + port );
 });
 
